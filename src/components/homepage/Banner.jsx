0d9eaeb7a1f5deaa199caff773ad15a3d7141469
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-25 03:16:09 +0600

    added banner section mobile responsive

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -7,26 +7,26 @@ const Banner = () => {
     return (
         <div className="relative">
             <div>
-                <div className="avatar absolute left-40 top-4">
-                    <div className="w-20 rounded-full">
+                <div className="avatar absolute left-6  md:left-40 top-20 md:top-4">
+                    <div className="w-10 md:w-20 rounded-full">
                         <img src={user1} />
                     </div>
                 </div>
                 {/* 2 */}
-                <div className="avatar absolute right-40 top-4 ">
-                    <div className="w-20 rounded-full">
+                <div className="avatar absolute right-6 md:right-40 top-20 md:top-4 ">
+                    <div className=" w-10 md:w-20 rounded-full">
                         <img src={user2} />
                     </div>
                 </div>
                 {/* 3 */}
-                <div className="avatar absolute bottom-6 left-4">
-                    <div className="w-24 rounded-full">
+                <div className="avatar absolute bottom-10 md:bottom-6 left-3 md:left-4">
+                    <div className="w-14 rounded-full">
                         <img src={user4} />
                     </div>
                 </div>
                 {/* 4 */}
-                <div className="avatar absolute bottom-4 right-10">
-                    <div className="w-24 rounded-full">
+                <div className="avatar absolute bottom-8 md:bottom-4 right-3 md:right-10">
+                    <div className="w-14 rounded-full">
                         <img src={user3} />
                     </div>
                 </div>
@@ -34,40 +34,23 @@ const Banner = () => {
 
             <div className="hero p-10 my-10">
                 <div className="hero-content text-center">
-                    <div className="max-w-lg p-10">
-                        <h1 className="text-4xl font-semibold">
+                    <div className="max-w-lg md:p-10">
+                        <h1 className="text-2xl md:text-4xl font-semibold">
                             Join us & Explore Thousands of Freelancer
                         </h1>
                         <p className="py-6 text-sm">
                             Work with talented people at the most affordable
                             price to get the most out of your time and cost
                         </p>
-                        <label className="input">
-                            <svg
-                                className="h-[1em] opacity-50"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            >
-                                <g
-                                    strokeLinejoin="round"
-                                    strokeLinecap="round"
-                                    strokeWidth="2.5"
-                                    fill="none"
-                                    stroke="currentColor"
-                                >
-                                    <circle cx="11" cy="11" r="8"></circle>
-                                    <path d="m21 21-4.3-4.3"></path>
-                                </g>
-                            </svg>
+                        <div className="join">
                             <input
-                                type="search"
-                                required
+                                className="input join-item"
                                 placeholder="what are you looking for?"
                             />
-                        </label>
-                        <button className="btn join-item rounded-r-full btn-primary">
-                            Subscribe
-                        </button>
+                            <button className="btn join-item rounded-r-full btn-primary">
+                                search
+                            </button>
+                        </div>
                         <p className="text-sm my-4">
                             Popular Searches : Designer, Developer, Web
                         </p>
